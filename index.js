0d3248d304f212defe53a86f86de75ab7a6e9234
Peter Jones <kephail@me.com>
2015-11-19 17:58:13 +0000

    added react

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 var express = require('express'),
+    reactViews = require('express-react-views'),
     app = express(),
     port = 3000;
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
 // Set up Routes for the application
 require('./app/routes/routes.js')(app);
+
+//Route not found -- Set 404
+app.get('*', function(req, res) {
+    res.json({
+        'route': 'Sorry this page does not exist'
+    });
+});
 
 app.listen(port);
 console.log('app running on port: ' + port);
